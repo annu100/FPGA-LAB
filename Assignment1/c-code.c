1d5700bfdc04b3d2c8d51ccda520eaa21ee9f687
ANNU <75775352+annu100@users.noreply.github.com>
2022-01-15 01:33:44 +0530

    Add files via upload

---
+++ Assignment1/c-code.c
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+ANNU,EE21RESCH01010
+*******************************************************************************/
+#include <stdio.h>
+
+int main(){
+    unsigned char X; //output
+    unsigned char one = 0x01;//used for displaying the output in bit
+    unsigned char  A=0x00,B=0x00,C=0x01,D=0x01; //inputs in hex
+    //Calculated POS EXPRESSION IS
+    X=((A|D)&((~A)|B)&((~B)|(~C))&(B|C|(A))&(A|C|(~D)));
+    printf("%x%x%x%x",one&A,one&B,one&C,one&D);
+    printf(" ");
+    printf("%x\n" ,one&X);
+    return 0;
+}
