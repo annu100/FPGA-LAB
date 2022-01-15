49432bfc1aa9922839b37c6698de1f10c7abc3cb
ANNU <75775352+annu100@users.noreply.github.com>
2022-01-16 02:56:48 +0530

    Update c-code.c

--- Assignment1/c-code.c
+++ Assignment1/c-code.c
@@ -1,19 +1,29 @@
 /******************************************************************************
 
-ANNU,EE21RESCH01010
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
 *******************************************************************************/
 *******************************************************************************/
 #include <stdio.h>
 #include <stdbool.h>
 
-// function for NOR operation
+// function for NAND  operation
 bool NAND(bool x,bool y){
     bool z=!(x && y);
     return z;
 }
+bool NAND2(bool x,bool y, bool z)
+{
+    bool r=!(x&&y&&z);
+    return r;
+}
 
 int main() {
     unsigned char X; //output
+    unsigned char X_; //output
     unsigned char one = 0x01;//used for displaying the output in bit
     unsigned char  A=0x00,B=0x00,C=0x01,D=0x01; //inputs in hex
     //Calculated POS EXPRESSION IS
@@ -21,13 +31,14 @@ int main() {
     printf("%x%x%x%x",one&A,one&B,one&C,one&D);
     printf(" ");
     printf("%x\n" ,one&X);
-    return 0;
+
 
   
 
   //Implementation of POS using NAND gate
-   X=(NAND(NAND(A,B),(~C)),NAND((~A),NAND((~B),NAND(C,D))));
-   printf("OUTPUT:%d",X);
+   //X_=((NAND(NAND(A,B),(~C)),NAND((~A),NAND((~B),NAND(C,D))));
+   X_=NAND((!A),(!B))&NAND(A,(!B))&NAND(B,C)&NAND2((!A),(!B),(!C))&NAND2((!A),(!C),D);
+   printf("OUTPUT:%d",X_);
 
 
 return 0;
